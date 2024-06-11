0d6de22a65112b627825c476fb2e9b49d058cf63
sintuepsoft <sintu_kumar@eplanetsoft.com>
2024-06-11 13:21:14 +0530

    parcel_image

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app.views import *
 urlpatterns = [
     path('dummy/',view_dummy,name='dummy'),
     path('parcel/', ParcelDetailView.as_view(), name='parcel-detail'),
+    path('parcel_image/', ParcelImageView.as_view(), name='parcel-image'),
 ]
